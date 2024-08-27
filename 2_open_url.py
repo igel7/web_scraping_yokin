660f9f668c2bc5477d2c3280c0cf48c49efafbfc
igel7 <168667349+igel7@users.noreply.github.com>
2024-08-27 10:32:34 +0900

    2_open_url.py の更新

--- 2_open_url.py
+++ 2_open_url.py
@@ -10,7 +10,7 @@ os.chdir(target_dir)
 print(f"Current working directory: {os.getcwd()}")
 
 # import CSV file
-input_csv = 'banks_output.csv'  # 入力CSVファイルの名前
+input_csv = 'banks_output.csv'
 banks_df = pd.read_csv(input_csv)
 
 # driver as global variant
@@ -18,7 +18,7 @@ driver = webdriver.Chrome(options=options)
 
 # setup
 options = Options()
-options.headless = False  # ヘッドレスモードをオフにする（ブラウザを表示する）
+options.headless = False  # shows browser
 
 # open URL 10 by 10
 def open_urls_in_batches(urls, batch_size=10):
@@ -27,14 +27,14 @@ def open_urls_in_batches(urls, batch_size=10):
         batch = urls[i:i + batch_size]
         for url in batch:
             driver.execute_script(f'window.open("{url}", "_blank");')
-            time.sleep(1)  # 少し待機して次のURLを開く
+            time.sleep(1)  # wait a second here
         print(f"Opened batch {i // batch_size + 1}")
-        input("Press Enter to close this batch and open the next one...")  # 次のバッチを開く前にEnterキーを待つ
+        input("Press Enter to close this batch and open the next one...") 
         driver.quit()
         driver = webdriver.Chrome(options=options)  # 新しいブラウザウィンドウを開く
 
 # import URL list
-urls = banks_df['url'].dropna().tolist()  # NaN値（URLが取得できなかった行）を除外する
+urls = banks_df['url'].dropna().tolist()  # remove NaN row（URLが取得できなかった行を除外)
 
 # open 10 URLs at once
 open_urls_in_batches(urls, batch_size=10)
